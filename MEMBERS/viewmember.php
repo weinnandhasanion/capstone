0ae0b4ea2705aa899f71e863183388e65958575e
weinnandhasanion <weinnandhasanion@gmail.com>
2021-02-09 19:30:37 +0800

    modified view member

--- MEMBERS/viewmember.php
+++ MEMBERS/viewmember.php
@@ -8,6 +8,11 @@ $sql = "SELECT * FROM `member` WHERE `member_id` = " . intval($id) . "";
 $res = mysqli_query($conn, $sql);
 
 if($row = mysqli_fetch_assoc($res)) {
+  $getProgramName = "SELECT program_name FROM program WHERE program_id = '".$row["program_id"]."'";
+  $result = mysqli_query($conn, $getProgramName);
+  $prog = mysqli_fetch_assoc($result);
+
+  $row["program_name"] = $prog["program_name"];
   echo json_encode($row);
 }
 ?>
