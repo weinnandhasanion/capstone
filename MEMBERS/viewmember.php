d3bd9a714ab214f9608b1b5e3b4296f5f2825c6f
weinnandhasanion <weinnandhasanion@gmail.com>
2021-02-09 19:36:35 +0800

    view member inner join query

--- MEMBERS/viewmember.php
+++ MEMBERS/viewmember.php
@@ -4,15 +4,14 @@ require('connect.php');
 
 $id = $_REQUEST['id'];
 
-$sql = "SELECT * FROM `member` WHERE `member_id` = " . intval($id) . "";
+$sql = "SELECT member.*, program.program_name 
+        FROM member 
+        INNER JOIN program 
+        ON member.program_id = program.program_id
+        WHERE member_id = " . intval($id) . "";
 $res = mysqli_query($conn, $sql);
 
 if($row = mysqli_fetch_assoc($res)) {
-  $getProgramName = "SELECT program_name FROM program WHERE program_id = '".$row["program_id"]."'";
-  $result = mysqli_query($conn, $getProgramName);
-  $prog = mysqli_fetch_assoc($result);
-
-  $row["program_name"] = $prog["program_name"];
   echo json_encode($row);
 }
 ?>
