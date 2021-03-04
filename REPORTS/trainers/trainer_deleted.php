9d2ccbb05daab700086e9e859643a627da656ea1
klintjohncagot <klintjohn60@gmail.com>
2021-03-05 02:32:27 +0800

    print reports for trainers and promos

--- REPORTS/trainers/trainer_deleted.php
+++ REPORTS/trainers/trainer_deleted.php
@@ -12,20 +12,20 @@ if($timespan == "Custom") {
   $toDate = NULL;
 }
 
-
+$reportTitle = "List of Deleted Trainers";
   if($timespan == "Custom") {
     $reportText = "Generating reports for trainers deleted from ".date("F d, Y", strtotime($fromDate))." to ".date("F d, Y", strtotime($toDate))."...";
     $sql = "SELECT * FROM trainer
             WHERE date_deleted >= '$fromDate' 
             AND date_deleted <= '$toDate'
-            AND acc_status = 'disable'";
+            AND trainer_status = 'deleted'";
     $res = mysqli_query($conn, $sql);
   } else if($timespan == "Today") {
     $reportText = "Generating reports for trainers deleted today, ".date("F d, Y")."...";
     $today = date("Y-m-d");
     $sql = "SELECT * FROM trainer
             WHERE date_deleted = '$today'
-            AND acc_status = 'disable'";
+            AND trainer_status = 'deleted'";
     $res = mysqli_query($conn, $sql);
   } else if($timespan == "This week") {
     $today = date("Y-m-d");
@@ -34,7 +34,7 @@ if($timespan == "Custom") {
     $sql = "SELECT * FROM trainer
             WHERE date_deleted >= '$lastWeek'
             AND date_deleted <= '$today'
-            AND acc_status = 'disable'";
+            AND trainer_status = 'deleted'";
     $res = mysqli_query($conn, $sql);
   } else if($timespan == "This month") {
     $monthStart = date("Y-m-01");
@@ -43,7 +43,7 @@ if($timespan == "Custom") {
     $sql = "SELECT * FROM trainer
             WHERE date_deleted >= '$monthStart'
             AND date_deleted <= '$monthEnd'
-            AND acc_status = 'disable'";
+            AND trainer_status = 'deleted'";
     $res = mysqli_query($conn, $sql);
   } else if($timespan == "This year") {
     $yearStart = date("Y-01-01");
@@ -52,57 +52,39 @@ if($timespan == "Custom") {
     $sql = "SELECT * FROM trainer
             WHERE date_deleted >= '$yearStart'
             AND date_deleted <= '$yearEnd'
-            AND acc_status = 'disable'";
+            AND trainer_status = 'deleted'";
     $res = mysqli_query($conn, $sql);
   } else {
     $reportText = "Generating reports for trainers deleted since all of time...";
-    $sql = "SELECT * FROM trainer WHERE  acc_status = 'disable'";
+    $sql = "SELECT * FROM trainer WHERE  trainer_status = 'deleted'";
     $res = mysqli_query($conn, $sql);
   }
 
+  
+  $data = array();
+  if($res) {
+    while($row = mysqli_fetch_assoc($res)) {
+      $row["name"] = $row["first_name"]." ".$row["last_name"];
+      $row["date_hired"] = date("M d, Y", strtotime($row["date_hired"]));
+      $row["date_deleted"] = date("M d, Y", strtotime($row["date_deleted"]));
+      $data[] = $row;
+    }
+  }
+  $labels = array("Trainer ID", "Name", "Email", "Date Registered","Date Deleted","Time Deleted");
+  $rowLabels = array("trainer_id", "name", "email", "date_hired","date_deleted","time_deleted");
+  
+  $object = (object) [
+    'data' => $data,
+    'rowLabels' => $rowLabels,
+    'labels' => $labels,
+    'toDate' => $toDate,
+    'fromDate' => $fromDate,
+    'reportTitle' => $reportTitle,
+    'fileName' => "ReportTrainersDeleted_".date("MdY")
+  ];
+  
+  $_SESSION["reports"] = $object;
+  header("Location: ./../print_reports.php");
+  exit;
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <p><?= $reportText ?></p>
-  <table>
-    <thead>
-      <tr>
-        <th>Trainer ID</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Date Deleted</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php 
-      if($res) {
-        if(mysqli_num_rows($res) > 0) {
-          while($row = mysqli_fetch_assoc($res)) {
-            ?>
-            <tr>
-              <td><?= $row["trainer_id"] ?></td>
-              <td><?= $row["first_name"] ?></td>
-              <td><?= $row["last_name"] ?></td>
-              <td><?= date("F d, Y", strtotime($row["date_deleted"])) ?></td>
-            </tr>
-            <?php
-            }
-        } else {
-          echo "Empty";
-        }
-      } else {
-        echo mysqli_error($conn);
-      }
-      ?>
-    </tbody>
-  </table>
-</body>
-</html>
