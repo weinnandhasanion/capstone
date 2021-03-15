c13729b452dedf33f7ba9826417c1f378928791d
weinnandhasanion <weinnandhasanion@gmail.com>
2021-03-15 23:13:06 +0800

    added date validation for member reports

--- MEMBERS/members.php
+++ MEMBERS/members.php
@@ -2222,8 +2222,6 @@
   <script type="text/javascript" src="js/popper.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <script type="text/javascript" src="js/mdb.min.js"></script>
-  <!--Google Maps-->
-  <script src="https://maps.google.com/maps/api/js"></script>
   <script type="text/javascript" src="validation.js"></script>
 
   <script>
