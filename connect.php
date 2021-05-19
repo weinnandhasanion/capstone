6d7cf1843a5924610b0ec460da45b5b1a4e6eab9
weinnandhasanion <weinnandhasanion@gmail.com>
2021-05-19 18:12:01 +0800

    reassigned credentials

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
 date_default_timezone_set('Asia/Manila');
-$conn = mysqli_connect("localhost","u154895212_cfgym","Tsukimoy1","u154895212_gym");
+$conn = mysqli_connect("localhost","root","","gym");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
