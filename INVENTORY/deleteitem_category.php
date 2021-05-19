dbbe639d3289282e62badffe53d0af95dcc13f1d
weinnandhasanion <weinnandhasanion@gmail.com>
2021-05-19 19:49:09 +0800

    delete all items under category

--- INVENTORY/deleteitem_category.php
+++ INVENTORY/deleteitem_category.php
@@ -11,8 +11,12 @@ if($res) {
   if(mysqli_num_rows($res) <= 0) {
     echo json_encode("Cannot delete category. There are existing items in this category.");
   } else {
+    $set = "SET foreign_key_checks = 0";
+    $res1 = mysqli_query($conn, $set);
     $sql = "DELETE FROM inventory WHERE category_id = $id";
     $res = mysqli_query($conn, $sql);
+    $set = "SET foreign_key_checks = 1";
+    $res1 = mysqli_query($conn, $set);
 
     if($res) {
       echo json_encode("success");
