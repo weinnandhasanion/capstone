80e6f1b7e9e4ee3ab87112097aee81af2d400a97
weinnandhasanion <weinnandhasanion@gmail.com>
2021-05-19 19:50:02 +0800

    hosting credentials

--- mobile/functions/connect.php
+++ mobile/functions/connect.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
 date_default_timezone_set('Asia/Manila');
-$con = new mysqli("localhost", "root", "", "gym");
+$con = new mysqli("localhost","u154895212_cfgym","Tsukimoy1","u154895212_gym");
 
 if(mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
